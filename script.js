7bb3a09f09a6670fb36d9f3d43bc37144b164f31
Seoyeon Choi <90433858+sadie0925@users.noreply.github.com>
2022-01-31 06:32:43 +0000

    Creating Checkbox and line-through

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 let container= document.getElementById('Goals');
 let input = document.getElementById('field')
 
+
 function createNewGoal(){
   var div = document.createElement("div");
-  div.class = "goal";
-  div.innerHTML = input.value;
+  div.className = "goal";
+  // div.innerHTML = input.value;
   var newCheckbox = document.createElement("input");
-  newCheckbox.type = "checkBox";
+  newCheckbox.id = "box";
+  newCheckbox.type = "checkbox";
+  var newLabel = document.createElement("Label");
+  newLabel.setAttribute("for", "box");
+  newLabel.innerHTML = input.value;
+
+  // var tasks = document.querySelectorAll(.tasks);
+  // for (var i = 0; i < tasks.length; i++) {
+  //   tasks[i].onclick = function() {
+  //     this. classList.toggle('completed')
+  //   }
+  // }
+  // div. createNewGoal ('click',function(){
+  // div. style. textDecoration = "line-through"
   div.appendChild(newCheckbox);
+  div.appendChild(newLabel);
   container.append(div);
+  // }
   // var Checkbox = document.getElementById('Checkbox').checked
 //  } else {
 //     text.style.display = "none";
